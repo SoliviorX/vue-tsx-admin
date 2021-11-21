3bd5d6e268f02b3cd3f7736188f6ee781d4bc02c
shideshan <307042873@qq.com>
2021-11-22 01:02:10 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import viteSvgIcons from 'vite-plugin-svg-icons';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), viteSvgIcons({
+    // 指定需要缓存的图标文件夹
+    iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+    // 指定symbolId格式
+    symbolId: 'icon-[dir]-[name]',
+  }),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "i18n": "vue-i18n/dist/vue-i18n.common.js"
+    }
+  },
+  build: {
+    // 禁用 brotli 压缩大小报告。
+    brotliSize: false,
+    // chunk 大小警告的限制
+    chunkSizeWarningLimit: 1024
+  },
+  server: {
+    hmr: {
+      overlay: false
+    },
+    port: 3000,
+    open: false,
+    https: false,
+    proxy: {}
+  }
+})
